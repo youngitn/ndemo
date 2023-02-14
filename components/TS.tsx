57a277ecb936100b916e41164bc6ef5b457e9aa0
TOPKEY\ytcheng <ytcheng@topkey.com.tw>
2023-02-14 10:13:08 +0800

    無限執行getData呼叫修正

--- components/TS.tsx
+++ components/TS.tsx
@@ -37,7 +37,7 @@ const DataTable = () => {
             setTableData(retData);
         });
         //return () => { alert('ok') }
-    }, [tableData])
+    }, [])
     const handleCreateNewRow = (values: DisableMaterialData) => {
         tableData.push(values);
         setTableData([...tableData]);
